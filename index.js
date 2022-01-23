56f61ed90f33106e98271a6e23d97ea1e0cc1368
Gisela <gngariboldi@gmail.com>
2022-01-23 11:34:59 -0300

    juego

---
+++ index.js
@@ -0,0 +1,11 @@
+function muestraPlaya(src) {
+    var playa = document.createElement("img");
+    img.src = 'imagenes/playa.jpg';
+    document.body.appendChild(playa);
+}
+
+function muestraMontaña(src) {
+    var montaña = document.createElement("img");
+    img.src = 'imagenes/montaña.jpg';
+    document.body.appendChild(montaña);
+}
